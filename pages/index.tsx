9aa00f1722c61724066a6b2ca3dc3cbe308340c4
Matthew Stanciu <mattbstanciu@gmail.com>
2022-10-11 23:57:48 -0400

    remove unused import

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,4 @@
 import Head from 'next/head'
-import Image from 'next/image'
 import { compareDesc, format, parseISO } from 'date-fns'
 import { allPosts, Post } from 'contentlayer/generated'
 import Link from 'next/link'
