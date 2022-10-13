60b78635cd3317b5fc973f2d5fe93d2e556a85fe
Matthew Stanciu <mattbstanciu@gmail.com>
2022-10-13 10:08:13 -0400

    remove return

--- pages/index.tsx
+++ pages/index.tsx
@@ -16,41 +16,37 @@ const PostCard = (post: Post) => {
   )
 }
 
-const Home = ({ posts }: { posts: Post[] }) => {
-  return (
-    <div className="min-h-screen">
-      <Head>
-        <title>Blog — Purdue Hackers</title>
-      </Head>
-      <div className="bg-cyan-100 border-b-4 border-black">
-        <div className="text-center py-16 sm:py-20 flex flex-col gap-y-4 items-center">
-          <h1 className="text-5xl sm:text-7xl font-bold">
-            Purdue Hackers Blog
-          </h1>
-          <h2 className="text-2xl text-gray-600">
-            Where we share our magic with the world ✨💛⚡️
-          </h2>
-        </div>
-      </div>
-      <div className="w-11/12 sm:max-w-md mx-auto rounded border-2 border-black shadow-blocks shadow-gray-800 p-4 mt-8 flex flex-col align-center gap-y-4">
-        {posts.map((post, idx) => (
-          <div className="grid grid-cols-2">
-            <time
-              dateTime={post.date}
-              className="text-base flex items-center text-gray-600 font-bold"
-            >
-              {format(
-                parseISO(post.date.substring(0, post.date.length - 14)),
-                'LLL d, yyyy'
-              )}
-            </time>
-            <PostCard key={idx} {...post} />
-          </div>
-        ))}
+const Home = ({ posts }: { posts: Post[] }) => (
+  <div className="min-h-screen">
+    <Head>
+      <title>Blog — Purdue Hackers</title>
+    </Head>
+    <div className="bg-cyan-100 border-b-4 border-black">
+      <div className="text-center py-16 sm:py-20 flex flex-col gap-y-4 items-center">
+        <h1 className="text-5xl sm:text-7xl font-bold">Purdue Hackers Blog</h1>
+        <h2 className="text-2xl text-gray-600">
+          Where we share our magic with the world ✨💛⚡️
+        </h2>
       </div>
     </div>
-  )
-}
+    <div className="w-11/12 sm:max-w-md mx-auto rounded border-2 border-black shadow-blocks shadow-gray-800 p-4 mt-8 flex flex-col align-center gap-y-4">
+      {posts.map((post, idx) => (
+        <div className="grid grid-cols-2">
+          <time
+            dateTime={post.date}
+            className="text-base flex items-center text-gray-600 font-bold"
+          >
+            {format(
+              parseISO(post.date.substring(0, post.date.length - 14)),
+              'LLL d, yyyy'
+            )}
+          </time>
+          <PostCard key={idx} {...post} />
+        </div>
+      ))}
+    </div>
+  </div>
+)
 
 export async function getStaticProps() {
   const posts: Post[] = allPosts.sort((a: Post, b: Post) => {
