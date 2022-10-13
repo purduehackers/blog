eb9de1a2937db0be00d4cc8dc7a74d14e296a588
Matthew Stanciu <mattbstanciu@gmail.com>
2022-10-12 23:16:28 -0400

    highlight color & remove log

--- components/author.tsx
+++ components/author.tsx
@@ -2,7 +2,6 @@ import parseMarkdownLink from 'lib/parse-markdown-link'
 
 const Author = ({ authorString }: { authorString: string }) => {
   const { author, avatarUrl } = parseMarkdownLink(authorString)
-  console.log(author, avatarUrl)
   return (
     <span className="flex flex-row gap-x-1 items-center">
       <img src={avatarUrl} className="w-[25px] rounded-full" />
