bbda7593e37c697012f2eb39337ed18ace34b972
Matthew Stanciu <mattbstanciu@gmail.com>
2022-10-14 18:44:07 -0400

    Add clock & `code` style

---
+++ components/posts/lightning-time/clock.tsx
@@ -0,0 +1,10 @@
+const Clock = () => (
+  <div className="w-full">
+    <iframe
+      src="https://lightning-time.vercel.app"
+      className="w-full aspect-video rounded-lg"
+    ></iframe>
+  </div>
+)
+
+export default Clock
