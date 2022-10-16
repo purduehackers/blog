00477a5136ddb11afc35eb7caa583b77c0971a19
Matthew Stanciu <mattbstanciu@gmail.com>
2022-10-16 17:28:31 -0400

    Update `@purduehackers/time` & code accordingly

--- components/posts/lightning-time/lightning-time-context.tsx
+++ components/posts/lightning-time/lightning-time-context.tsx
@@ -18,16 +18,13 @@ const LightningTimeProvider = ({ ...props }) => {
       const convertedTime = lt.convertToLightning(now).lightningString
       const colors = Object.values(lt.getColors(convertedTime))
 
-      document.documentElement.style.setProperty('--boltColor', `#${colors[0]}`)
-      document.documentElement.style.setProperty('--zapColor', `#${colors[1]}`)
-      document.documentElement.style.setProperty(
-        '--sparkColor',
-        `#${colors[2]}`
-      )
+      document.documentElement.style.setProperty('--boltColor', colors[0])
+      document.documentElement.style.setProperty('--zapColor', colors[1])
+      document.documentElement.style.setProperty('--sparkColor', colors[2])
 
-      setBoltColor(`#${colors[0]}`)
-      setZapColor(`#${colors[1]}`)
-      setSparkColor(`#${colors[2]}`)
+      setBoltColor(colors[0])
+      setZapColor(colors[1])
+      setSparkColor(colors[2])
     }, 100)
     return () => clearInterval(timer)
   }, [])
