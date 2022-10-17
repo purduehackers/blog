0babdca6b2c0e033abccd9c4e525ed422fd29027
Matthew Stanciu <mattbstanciu@gmail.com>
2022-10-17 17:15:43 -0400

    Make the footer a little bit bigger

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,5 +1,5 @@
 const Footer = ({ ...props }) => (
-  <div className="bg-gray-200 text-center bottom-0 mt-auto w-full flex flex-col justify-center gap-y-4 py-8 px-4">
+  <div className="bg-gray-200 text-center bottom-0 mt-auto w-full flex flex-col justify-center gap-y-4 py-12 px-4">
     {props.children}
   </div>
 )
