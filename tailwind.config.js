ea4068cf89671765b88f3fadec7afa4cca5520eb
Matthew Stanciu <mattbstanciu@gmail.com>
2022-10-17 00:46:22 -0400

    Fix tailwind not finding mdx files

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,7 @@ module.exports = {
   content: [
     './pages/**/*.{js,ts,jsx,tsx}',
     './components/**/*.{js,ts,jsx,tsx}',
-    './posts/*.{mdx}',
+    './posts/*.mdx',
     './lib/*.{js,ts}'
   ],
   theme: {
