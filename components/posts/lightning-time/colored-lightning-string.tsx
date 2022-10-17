0587c321e3de017e545ecad41554c3fe478ce970
Matthew Stanciu <mattbstanciu@gmail.com>
2022-10-17 19:04:53 -0400

    Make all parts of colored lightning string bold

--- components/posts/lightning-time/colored-lightning-string.tsx
+++ components/posts/lightning-time/colored-lightning-string.tsx
@@ -2,7 +2,7 @@ import LightningText, { LightningPart } from './lightning-text'
 
 const ColoredLightningString = () => {
   return (
-    <span className="font-mono">
+    <span className="font-mono font-bold">
       <LightningText part={LightningPart.Bolt} />~
       <LightningText part={LightningPart.Zap} />~
       <LightningText part={LightningPart.Spark} />|
