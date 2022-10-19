1d71d663ec11dcc183f5c79852c4367ceb34746e
Matthew Stanciu <mattbstanciu@gmail.com>
2022-10-18 22:11:31 -0400

    Add convert to lightning

---
+++ components/posts/lightning-time/convert-to-lightning.tsx
@@ -0,0 +1,45 @@
+import { LightningTime } from '@purduehackers/time'
+import { DateTime } from 'luxon'
+import { useState } from 'react'
+
+const ConvertToLightning = () => {
+  const lt = new LightningTime()
+  const [lightningString, setLightningString] = useState('8~0~0')
+  const [colors, setColors] = useState(['#80a100', '#3200d6', '#f68500'])
+  return (
+    <div
+      className="p-1 rounded mx-auto font-main w-full md:w-1/2"
+      style={{
+        background: `linear-gradient(to right, ${colors[0]}, ${colors[1]}, ${colors[2]})`
+      }}
+    >
+      <div className="rounded p-4 flex flex-col justify-center bg-white">
+        <p className="text-xl text-center font-bold">
+          Convert to Lightning Time
+        </p>
+        <input
+          type="time"
+          step={1}
+          className="pl-1 border border-black rounded mt-4 w-7/12 mx-auto"
+          defaultValue="12:00"
+          onChange={(e) => {
+            if (e.target.value) {
+              const date = DateTime.fromISO(e.target.value).toJSDate()
+              const { lightningString } = lt.convertToLightning(date)
+              const newColors = Object.values(lt.getColors(lightningString))
+              setColors(newColors)
+              setLightningString(lightningString)
+            }
+          }}
+        ></input>
+        <div className="text-center mt-2">
+          <p className="text-xl font-bold font-mono">
+            ⚡️ {lightningString} ⚡️
+          </p>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default ConvertToLightning
