f7a1bd85d1142e61381d97726420a2a526064cbc
Matthew Stanciu <mattbstanciu@gmail.com>
2022-12-12 21:51:22 -0500

    Fix card a tag

--- components/post-card.tsx
+++ components/post-card.tsx
@@ -5,20 +5,22 @@ import Link from 'next/link'
 const PostCard = (post: Post) => {
   return (
     <Link href={post.url}>
-      <div
-        className="flex flex-col justify-center text-center p-4 rounded-lg border-2 border-black cursor-pointer"
-        style={{ backgroundColor: post.color.bg }}
-      >
-        <time dateTime={post.date} className="text-sm text-gray-600">
-          {format(
-            parseISO(post.date.substring(0, post.date.length - 14)),
-            'LLL d, yyyy'
-          )}
-        </time>
-        <h2 className="text-3xl sm:text-2xl md:text-3xl font-bold">
-          {post.title}
-        </h2>
-      </div>
+      <a>
+        <div
+          className="flex flex-col h-full justify-center text-center p-4 rounded-lg border-2 border-black"
+          style={{ backgroundColor: post.color.bg }}
+        >
+          <time dateTime={post.date} className="text-sm text-gray-600">
+            {format(
+              parseISO(post.date.substring(0, post.date.length - 14)),
+              'LLL d, yyyy'
+            )}
+          </time>
+          <h2 className="text-3xl sm:text-2xl md:text-3xl font-bold">
+            {post.title}
+          </h2>
+        </div>
+      </a>
     </Link>
   )
 }
