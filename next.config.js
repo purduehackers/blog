d377fbc6aaa3ab476e5aaea8ab9266f774d2c1a6
Matthew Stanciu <mattbstanciu@gmail.com>
2023-11-07 16:30:59 -0500

    use `remotePatterns`

--- next.config.js
+++ next.config.js
@@ -7,7 +7,26 @@ const config = withContentlayer({
   swcMinify: true,
   pageExtensions: ['js', 'ts', 'tsx', 'mdx'],
   images: {
-    domains: ['github.com', 'cdn.discordapp.com', 'media.discordapp.net']
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'github.com',
+        port: '',
+        pathname: '/**'
+      },
+      {
+        protocol: 'https',
+        hostname: 'cdn.discordapp.com',
+        port: '',
+        pathname: '/**'
+      },
+      {
+        protocol: 'https',
+        hostname: 'media.discordapp.net',
+        port: '',
+        pathname: '/**'
+      }
+    ]
   },
   transpilePackages: ['geist']
 })
