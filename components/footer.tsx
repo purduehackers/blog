79e2a5aae767ddb644f6b8c8c3995ce9c26c0f7d
Arhan Chaudhary <arhan.ch@gmail.com>
2024-09-14 22:29:59 -0400

    Add RSS link

--- components/footer.tsx
+++ components/footer.tsx
@@ -6,11 +6,11 @@ const Footer = () => (
       Made with 💛 and ⚡️ •{' '}
       <span className="underline underline-offset-4 decoration-2">
         <a
-          href="https://github.com/purduehackers/blog"
+          href="https://blog.purduehackers.com/rss.xml"
           target="_blank"
           className="decoration-amber-400 dark:decoration-amber-500 hover:decoration-[3px]"
         >
-          Open source
+          RSS
         </a>
       </span>{' '}
       •{' '}
