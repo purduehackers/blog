569d3c5d3dceb493e71ff9e5fe8ee277ed14755a
Michael <theshadoweevee@shad.moe>
2024-10-31 21:16:49 -0400

    Change RSS URL

--- src/pages/rss.xml.ts
+++ src/pages/rss.xml.ts
@@ -28,7 +28,7 @@ export const GET: APIRoute = async ({ site }) => {
       `<pubDate>${sorted_posts[0]!.data.pubDate.toUTCString()}</pubDate>`,
     items: sorted_posts.map((post) => ({
       ...post.data,
-      link: `/blog/${post.slug}/`,
+      link: `/pages/${post.slug}/`,
     })),
   });
 };
