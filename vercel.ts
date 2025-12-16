563b4684b87946a3ba760d0d355a9ddd5010a49c
Matthew Stanciu <mattbstanciu@gmail.com>
2025-12-15 18:33:15 -0800

    remove quotes

--- vercel.ts
+++ vercel.ts
@@ -1,13 +1,13 @@
 import { routes, type VercelConfig } from '@vercel/config/v1';
 
 export const config: VercelConfig = {
-  "buildCommand": "pnpm run build",
-  "cleanUrls": true,
-  "devCommand": "pnpm run dev",
-  "framework": "astro",
-  "installCommand": "pnpm install",
-  "outputDirectory": "dist",
-  "trailingSlash": false,
+  buildCommand: "pnpm run build",
+  cleanUrls: true,
+  devCommand: "pnpm run dev",
+  framework: "astro",
+  installCommand: "pnpm install",
+  outputDirectory: "dist",
+  trailingSlash: false,
   redirects: [
     routes.redirect('/home', 'https://purduehackers.com')
   ],
